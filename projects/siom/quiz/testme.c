3669ca62bc3529146ebd808147c0b072315f2839
mchaelsio343 <siom@oregonstate.edu>
2019-07-26 21:47:40 +0800

    submittion

--- projects/siom/quiz/testme.c
+++ projects/siom/quiz/testme.c
@@ -5,30 +5,40 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
-    return ' ';
+    int i = (char) rand()%128;
+
+    return i;
 }
 
-char *inputString()
+char *inputString(char* strs)
 {
-    // TODO: rewrite this function
-    return "";
+
+    char charPool[] = {'d','e','f','q','r','s','t','u'};
+    int i;
+    for ( i = 0 ; i <= 4 ; i++){
+      char charPoolIndex = rand() % 8 ;
+      strs[i] = charPool[charPoolIndex];
+    }
+    strs[5] = '\0';
+ 
+    return strs;
 }
 
 void testme()
 {
   int tcCount = 0;
-  char *s;
+  char s[6];
   char c;
   int state = 0;
   while (1)
   {
     tcCount++;
     c = inputChar();
-    s = inputString();
+    inputString(s);
     printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
     if (c == '[' && state == 0) state = 1;
+
     if (c == '(' && state == 1) state = 2;
     if (c == '{' && state == 2) state = 3;
     if (c == ' '&& state == 3) state = 4;
@@ -37,6 +47,7 @@ void testme()
     if (c == '}' && state == 6) state = 7;
     if (c == ')' && state == 7) state = 8;
     if (c == ']' && state == 8) state = 9;
+
     if (s[0] == 'r' && s[1] == 'e'
        && s[2] == 's' && s[3] == 'e'
        && s[4] == 't' && s[5] == '\0'
